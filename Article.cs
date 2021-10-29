f946911015a1b1501085d3696c72416cbff6e3c6
nedimbatilovic <nedimco2006@gmail.com>
2021-10-29 21:34:28 +0200

    Add project files.

---
+++ Article.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS
+{
+    public class Article
+    {
+        private string _index;
+        public string Index
+        {
+            get => _index;
+            set
+            {
+                _index = value;
+            }
+        }
+        public string Name { get; set; }
+
+        private decimal _entryPrice;
+        public decimal EntryPrice { get; set; }
+
+        private decimal _outputPrice;
+        public decimal OutputPrice {
+            get => _outputPrice;
+            set
+            {
+                _outputPrice = value;
+                _margin = (int)(100 * (_outputPrice / _entryPrice - 1));
+            }
+        }
+
+        private int _margin;
+        public int Margin {
+            get => _margin;
+            set
+            {
+                _margin = value;
+                _outputPrice = 100 * (1 + Margin / 100);
+            } 
+        }
+
+        private int _tax;
+        public int Tax
+        {
+            get => _tax;
+            set
+            {
+                _tax = value;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace POS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Article> ArticleList = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Article();
+            ArticleDisplay.ItemsSource = ArticleList;
+            ArticleList.Add(new Article { Name="Voda", EntryPrice=10});
+            /*File.WriteAllText("test.txt", "stvari");
+            string jhg = "hghg" + Environment.NewLine;
+            string nesto = File.ReadAllText("test.txt");*/
+              
+        }
+
+        private void ArticleAddition(object sender, RoutedEventArgs e)
+        {
+            ArticleList.Add(DataContext as Article);
+            DataContext = new Article();
+        }
+    }
+}
